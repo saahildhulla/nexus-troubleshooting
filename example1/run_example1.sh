ca070ee03acb89d2dd2f12a86d59506ac07e6b98
Saahil Dhulla <saahil.dhulla@dominodatalab.com>
2023-01-09 23:51:45 -0500

    test

--- example1/run_example1.sh
+++ example1/run_example1.sh
@@ -7,6 +7,6 @@ kubectl config use-context $DEPLOY_ID
 password=$(kubectl get secret -n domino-platform mongodb-replicaset-admin -ojson | jq -r '.data.password' | base64 -d)
 
 # exec into mongo pod and mess with configuration
-kubectl exec -it -n domino-platform mongodb-replicaset-0 -- bash -c "mongo -u admin -p $password --authenticationDatabase admin domino --eval 'db.data_planes.updateOne({\"name\": {\$ne: \"\"}}, {\$set: {\"configuration.storageClass\": \"gp3\"}})'"
+kubectl exec -it -n domino-platform mongodb-replicaset-0 -- bash -c "mongo -u admin -p $password --authenticationDatabase admin domino --eval 'db.data_planes.updateOne({\"name\": {\$ne: \"\"}}, {\$set: {\"configuration.storageClass\": \"KevinSaysHi\"}})'"
 
 echo "Example 1 is set up!"
